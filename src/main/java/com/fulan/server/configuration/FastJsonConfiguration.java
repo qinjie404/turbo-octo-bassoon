36dddc270f32e3af109383220a3902b0c8585762
qinjie <qinjie@fulan.com.cn>
2019-11-06 14:23:40 +0800

    新增FastJson处理Json数据

---
+++ src/main/java/com/fulan/server/configuration/FastJsonConfiguration.java
@@ -0,0 +1,39 @@
+package com.fulan.server.configuration;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * FastJson配置类
+ * @author 13862
+ */
+@Configuration
+public class FastJsonConfiguration implements WebMvcConfigurer {
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        //创建FastJson消息转换器
+        FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
+        //创建配置类
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        //修改配置返回内容的过滤
+        fastJsonConfig.setSerializerFeatures(
+                //List字段如果为null,输出为[],而非null
+                SerializerFeature.WriteNullListAsEmpty,
+                //字符类型字段如果为null,输出为"",而非null
+                SerializerFeature.WriteNullStringAsEmpty,
+                //Boolean字段如果为null,输出为false,而非null
+                SerializerFeature.WriteNullBooleanAsFalse,
+                //是否输出值为null的字段,默认为false
+                SerializerFeature.WriteMapNullValue
+        );
+        fastJsonHttpMessageConverter.setFastJsonConfig(fastJsonConfig);
+        //将fastjson添加到试图消息转换器列表内
+        converters.add(fastJsonHttpMessageConverter);
+    }
+}

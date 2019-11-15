912bedeead972fb666e197478f74494b90b56042
qinjie <qinjie@fulan.com.cn>
2019-11-15 14:20:11 +0800

    新增定时任务的简单应用

--- src/main/java/com/fulan/server/FulanDemoApplication.java
+++ src/main/java/com/fulan/server/FulanDemoApplication.java
@@ -3,6 +3,7 @@ package com.fulan.server;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * 应用启动类
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication(scanBasePackages = "com.fulan.server")
 @MapperScan(basePackages = "com.fulan.server.dao")
+@EnableScheduling
 public class FulanDemoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/fulan/server/scheduletask/TestTask.java
@@ -0,0 +1,22 @@
+package com.fulan.server.scheduletask;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 定时任务
+ * @author 13862
+ */
+@Component
+@Slf4j
+public class TestTask {
+
+    @Scheduled(cron = "0/10 * * * * ? ")
+    public void cron(){
+        log.info("执行测试cron时间"+new SimpleDateFormat("HH:mm:ss").format(new Date()));
+    }
+}
